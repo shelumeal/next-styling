7042156309ab49306ae457d8576eb43ee1b76ba4
Shelumiel <sssharon7@gmail.com>
2022-09-14 16:05:02 +0800

    Add Global Styles and bootstrap styles

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+function about() {
+  return (
+    <div>
+      <h2>About page</h2>
+      <button className="btn btn-success">Primary</button>
+    </div>
+  );
+}
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+function contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  );
+}
+
+export default contact;
